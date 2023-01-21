9d5afa4882e02ee4c7e6ef518d23047db4bfc56f
EUIJOON <euijoonism@gmail.com>
2023-01-21 15:04:52 +0900

    if문 성인 청소년 판별

--- src/Prac03/Day03.java
+++ src/Prac03/Day03.java
@@ -1,6 +1,7 @@
 package Prac03;
 
 public class Day03 {
+    /*
     public static void main(String[] args) {
 
         int a = 31;
@@ -10,6 +11,18 @@ public class Day03 {
             System.out.println("100보다 작고 50보다 큰 수입니다.");
         } else {
             System.out.println("50보다 작습니다.");
+        }
+    }
+     */
+
+    public static void main(String[] args) {
+        //퀴즈
+
+        int age = 25;
+        if(age > 19) {
+            System.out.println("성인입니다");
+        } else {
+            System.out.println("청소년입니다.");
         }
     }
 }
