a2a175b9288fb1198ee39fd15115c78510baffae
EUIJOON <euijoonism@gmail.com>
2023-01-21 12:23:29 +0900

    [퀴즈 1] 세개의 숫자를 각각 변수 선언하고 세수의 총합을 인쇄해보기

---
+++ src/Prac01/Scanner.java
@@ -0,0 +1,16 @@
+package Prac01;
+
+public class Scanner {
+// 퀴즈 1
+// 세개의 숫자를 각각 변수 선언하고 세수의 총합을 인쇄해보기
+
+    public static void main(String[] args) {
+
+
+        int i = 10;
+        int j = 20;
+        int k = 3;
+        int sum = i+j+k;
+        System.out.println("세수의 합은 " + (i+j+k)+ " 입니다.");
+    }
+}
