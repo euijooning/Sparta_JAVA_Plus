72282771bf3cda2fa65b79dcb47b288c56a21e2d
EUIJOON <euijoonism@gmail.com>
2023-01-21 14:57:47 +0900

    숫자의 범위 판별 if - else if

--- src/Prac03/Day03.java
+++ src/Prac03/Day03.java
@@ -1,5 +1,15 @@
 package Prac03;
 
 public class Day03 {
+    public static void main(String[] args) {
 
+        int a = 31;
+        if (a > 100) {
+            System.out.println("100보다 큰 수입니다.");
+        } else if (a <= 100 && a > 50) {
+            System.out.println("100보다 작고 50보다 큰 수입니다.");
+        } else {
+            System.out.println("50보다 작습니다.");
+        }
+    }
 }
