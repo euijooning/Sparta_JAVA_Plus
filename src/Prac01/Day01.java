15ead0c9c4cba1d3d8f9c53c0e213c0537af27b6
EUIJOON <euijoonism@gmail.com>
2023-01-21 12:06:07 +0900

    java start

---
+++ src/Prac01/Day01.java
@@ -0,0 +1,28 @@
+package Prac01;
+
+public class Day01 {
+    public static void main(String[] args) {
+        /* 출력하기
+        원하는 값으로 출력하기
+        1) Hello World!
+        2) 안녕 자바!
+        3) 안녕
+           자바!
+        */
+
+        System.out.println("Hello World!");
+        System.out.println("------1-------");
+        System.out.println("안녕 자바!");
+        System.out.println("안녕\t자바");
+        System.out.println("------2-------");
+        System.out.println("안녕");
+        System.out.println("자바");
+        System.out.println("안녕 \n자바");
+        System.out.println("------3-------");
+
+
+    }
+}
+
+
+
