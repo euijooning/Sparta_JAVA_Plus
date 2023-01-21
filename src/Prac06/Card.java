aeb9201b3ce73fc010a915a9699517c3eba14882
EUIJOON <euijoonism@gmail.com>
2023-01-21 16:19:25 +0900

    [작업] Card 클래스 작성

--- src/Prac06/Card.java
+++ src/Prac06/Card.java
@@ -1,4 +1,23 @@
 package Prac06;
 
 public class Card {
+    public String user;
+    public int pw;
+    public int bal;
+
+    public Card(){;}
+
+    public Card(String user, int pw, int bal) {
+        this.user = user;
+        this.pw = pw;
+        this.bal = bal;
+    }
+    public int getPw() {
+        return pw;
+    }
+
+    public void setPw(int pw) {
+        this.pw = pw;
+        System.out.println("비밀번호 변경 완료!");
+    }
 }
