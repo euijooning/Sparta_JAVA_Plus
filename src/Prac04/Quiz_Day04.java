95c749d32907f423edfd5dd048e1a0ce38c41aaa
EUIJOON <euijoonism@gmail.com>
2023-01-21 15:50:23 +0900

    퀴즈 04. 구구단 출력하기 다양한 방법

--- src/Prac04/Quiz_Day04.java
+++ src/Prac04/Quiz_Day04.java
@@ -74,5 +74,30 @@ public class Quiz_Day04 {
                 System.out.println();// 단마다 개행을 위한 한 줄 비움
          }
     }
+    /*
+    public static void main(String[] args) {
+        for(int i = 2; i<10; i++) {
+            for(int j = 1; j<10 ; j++) {
+                System.out.print(i + " * " + j + " = " + (i*j) + "\t");
+            }
+            System.out.println();
+        }
+    }
+     */
+    /*
+
+    //좀 더 세련되게 구구단 출력하기 세로로
+    public static void main(String[] args) {
+        for(int i=2; i<=9; i++){
+            System.out.print("["+i+"단]\t");
+        }
+        System.out.println();
+        for(int i=1; i<=9; i++){
+            for(int j=2; j<=9; j++){
+                System.out.print(j+"*"+i+"="+j*i+"\t");
+            }System.out.println();
+        }
+    }
+     */
 }
 
