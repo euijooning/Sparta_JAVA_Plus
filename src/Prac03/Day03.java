3b593167ed1cdad0a4a397982bb396ee81640ab4
EUIJOON <euijoonism@gmail.com>
2023-01-21 15:17:14 +0900

    [if] Scanner 사용하여 윤년, 평년 판별

--- src/Prac03/Day03.java
+++ src/Prac03/Day03.java
@@ -76,7 +76,7 @@ public class Day03 {
     }
 
      */
-
+    /*
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -95,5 +95,25 @@ public class Day03 {
             default:
                 System.out.println("F입니다.");
         }
+    }
+     */
+
+    //퀴즈 : 윤년, 평년 판별 Scanner 이용
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("윤년, 평년을 판별할 년도를 입력하세요: ");
+        int year = scanner.nextInt();
+
+//    년도가 4로 나눠 떨어지고 100으로 나눠 떨어지지 않거나 400으로 나눠서 떨어지면 윤년,
+//    그렇지 않으면 평년
+        boolean isLeapYear = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+
+        if (isLeapYear) {
+            System.out.println(year + "년은 윤년입니다.");
+        } else {
+            System.out.println(year + "년은 평년입니다.");
+        }
+
+
     }
 }
