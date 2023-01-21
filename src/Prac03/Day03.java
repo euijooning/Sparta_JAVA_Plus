db04192ef921e9801dde47adf36ecde7e5c427dd
EUIJOON <euijoonism@gmail.com>
2023-01-21 15:18:37 +0900

    [switch] Scanner 사용하여 마지막 날짜를 출력할 년, 월을 입력

--- src/Prac03/Day03.java
+++ src/Prac03/Day03.java
@@ -97,7 +97,7 @@ public class Day03 {
         }
     }
      */
-
+/*
     //퀴즈 : 윤년, 평년 판별 Scanner 이용
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -113,7 +113,44 @@ public class Day03 {
         } else {
             System.out.println(year + "년은 평년입니다.");
         }
+    }
+ */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("마지막 날짜를 출력할 년, 월을 입력하세요: ");
+        int year = scanner.nextInt();
+        int month = scanner.nextInt();
+        int day;
 
+        switch (month) {
+            case 2:
+                day = year % 4 == 0 && year % 100 != 0 || year % 400 == 0 ? 29 : 28;
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                day = 30;
+                break;
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                day = 31;
+                break;
+            default:
+                day = 0;
+                break;
+        }
+        if (day == 0) {
+            System.out.println(month + "월은 존재하지 않는 달 입니다.");
+        } else {
+            System.out.println
+                    (year + "년 " + month + "월의 마지막 날짜는 " + day + "일 입니다.");
+        }
 
     }
 }
