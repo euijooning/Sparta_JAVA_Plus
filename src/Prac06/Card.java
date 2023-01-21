842e68666c3418866d402e31b89804743b9d6ed2
EUIJOON <euijoonism@gmail.com>
2023-01-21 16:31:45 +0900

    [퀴즈] 카드 만들기 프로그램 다시 시작

--- src/Prac06/Card.java
+++ src/Prac06/Card.java
@@ -1,4 +1,15 @@
 package Prac06;
 
 public class Card {
+    public String user;
+    public int pw;
+    public int bal;
+
+    public Card(){;}
+
+    public Card(String user, int pw, int bal) {
+        this.user = user;
+        this.pw = pw;
+        this.bal = bal;
+    }
 }
