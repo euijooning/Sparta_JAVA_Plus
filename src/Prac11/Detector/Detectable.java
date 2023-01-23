881d11e47a421b682c4648c0f45cca4bab0342f9
EUIJOON <euijoonism@gmail.com>
2023-01-23 14:25:05 +0900

    [추가] Detectabld의 변수 추가할 때 유의사항

--- src/Prac11/Detector/Detectable.java
+++ src/Prac11/Detector/Detectable.java
@@ -1,6 +1,12 @@
 package Prac11.Detector;
 
 public interface Detectable {
-    void detect();
+    // <추가> 2. 인터페이스 내에서 어떤 변수를 정의한다고 해보면,
+    // String name = "감지자";
+    // 앞에 추가되어있는 거 잘 생각하자
+    public static final String name = "감지자";
+
+   // <추가> 1. 사실 숨겨져 있던 게 있다.
+    public abstract void detect();
 
 }
