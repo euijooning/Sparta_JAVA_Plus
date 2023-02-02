28503390e48b8a65bb119aa4e8c3693ca6a6b380
EUIJOON <euijoonism@gmail.com>
2023-02-02 19:20:46 +0900

    HashSet 시작

---
+++ src/Prac12/user12/_06_HashSet.java
@@ -0,0 +1,12 @@
+package Prac12.user12;
+
+import java.util.HashSet;
+
+public class _06_HashSet {
+    public static void main(String[] args) {
+        //Set
+        HashSet<String> set = new HashSet<>();
+
+        // 삼겹살 구워먹기 위한 재료들을 추가한다.
+    }
+}
