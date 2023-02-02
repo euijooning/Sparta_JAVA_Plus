73f5d2bf69457cecca092086b9ab7de388c4d740
EUIJOON <euijoonism@gmail.com>
2023-02-02 19:42:52 +0900

    containsKey

--- src/Prac12/_07_HashMap.java
+++ src/Prac12/_07_HashMap.java
@@ -7,7 +7,7 @@ public class _07_HashMap {
         // 설명 보충 필요 다시 듣고 정리
 
         HashMap<String, Integer> map = new HashMap<>();
-                // Key는 자료형, Value값은 숫자기 때문에 Integer
+        // Key는 자료형, Value값은 숫자기 때문에 Integer
 
         // 데이터 추가 이거는 add가 아니라 put
         map.put("유재석", 10);
@@ -21,5 +21,17 @@ public class _07_HashMap {
         System.out.println("유재석 님의 포인트: " + map.get("유재석")); // 괄호 안에 적어주면 그 값을 얻어온다.
         System.out.println("유재석 님의 포인트: " + map.get("박명수"));
         System.out.println("--------------");
+
+        //확인
+        if (map.containsKey("서장훈")) {
+            int humanspoint = map.get("서장훈");
+            map.put("서장훈", ++humanspoint);
+            System.out.println("서장훈 님의 누적 포인트: " + map.get("서장훈"));
+        } else { // 서장훈이 리스트에 없는 경우
+            map.put("서장훈", 1);
+            System.out.println("서장훈님 (포인트 1점)");
+
+
+        }
     }
 }
