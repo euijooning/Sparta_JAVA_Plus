865c0d506fbe5b35ca18d33a25ae7762c129abd1
EUIJOON <euijoonism@gmail.com>
2023-02-02 19:34:37 +0900

    HashMap

---
+++ src/Prac12/_07_HashMap.java
@@ -0,0 +1,13 @@
+package Prac12;
+
+import java.util.HashMap;
+
+public class _07_HashMap {
+    public static void main(String[] args) {
+        // 설명 보충 필요
+
+        HashMap<String, Integer> map = new HashMap<>();
+                // Key는 자료형, Value값은 숫자기 때문에 Integer
+
+    }
+}
