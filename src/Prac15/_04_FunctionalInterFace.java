eda6ec3fa7600518f58bd90962f167229cddf61d
EUIJOON <euijoonism@gmail.com>
2023-02-12 01:18:59 +0900

    첫 번째 출력 예시

--- src/Prac15/_04_FunctionalInterFace.java
+++ src/Prac15/_04_FunctionalInterFace.java
@@ -10,6 +10,9 @@ public class _04_FunctionalInterFace {
 // 달러 금액을 입력하면, 우리 돈으로 얼마인지를 알려주는(변환해주는) 프로그램 만들기
 
   public static void main(String[] args) {
-
+    // KRWConverter 객체 만들어서 실행해보기
+    KRWConverter16 converter = new KRWConverter16();
+    converter.convert(1); // 1 달러 = 1200 원 출력
   }
+
 }
--- src/Prac15/converter/KRWConverter16.java
+++ src/Prac15/converter/KRWConverter16.java
@@ -3,8 +3,13 @@ package Prac15.converter;
 // USD 돈으로 우리 돈으로 얼마인지를 알려주는(변환해주는) 것을 구현해주는 클래스임
 public class KRWConverter16 implements Convertible16 {
 
+  // 환율은 1,200원이라고 가정하자.
   @Override
   public void convert(int USD) {
+    System.out.println(USD + " 달러 = " + (USD*1200) + " 원");
+    // 실행하면 "1 달러 = 1200 원" 이렇게 나올 듯.
+    // 이 클래스를 객체로 만들어서 한 번 실행해보겠다. Class _04_로 간다.
+
 
   }
 }
