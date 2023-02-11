b7eb01b2d6dbbc119a1d29a7588c34a6cb339a35
EUIJOON <euijoonism@gmail.com>
2023-02-12 01:26:39 +0900

    메서드 분리해서 converter.convert()가 하는 내용을 출력해보기

--- src/Prac15/_04_FunctionalInterFace.java
+++ src/Prac15/_04_FunctionalInterFace.java
@@ -12,7 +12,23 @@ public class _04_FunctionalInterFace {
   public static void main(String[] args) {
     // KRWConverter 객체 만들어서 실행해보기
     KRWConverter16 converter = new KRWConverter16();
-    converter.convert(1); // 1 달러 = 1200 원 출력
+    //converter.convert(1); // 1 달러 = 1200 원 출력
+
+    convertUSD(converter, 2); // 2 달러 = 2400 원 출력.
+    /*
+    첫 번째와 똑같은 결과가 나왔는데, 왜 그럴까?
+    => 너무 당연하다. 우리는 converter.convert(1);에서 하는 동작을 아래의 convertUSD() 메서드로 옮긴 것일 뿐이다.
+     */
+
+  }
+  // 아래에 메서드 분리해서 converter.convert()가 하는 내용을 출력해보자
+
+  // 우리가 지금 환율을 변환하는 메서드를 만드는 것이므로,
+  // 괄호 안에 인터페이스를 넣어주고(KRWConverter을 사용할 수도 있지만 다형성에 의해서 가능),
+  // 얼마의 달러를 어떻게 변환해줄거냐를 계산 시켜야 하므로 int로 USD 달러를 받는다.
+  public static void convertUSD(Convertible16 converter, int USD) { //원래는 convertiber16이어야 하지만 편의상  converter라고 썼음
+   converter.convert(USD);
+
   }
 
 }
