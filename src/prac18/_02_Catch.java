207aae8c82a97c6348fab399bdd2b12852d03a5d
EUIJOON <euijoonism@gmail.com>
2023-02-16 19:43:58 +0900

    try catch 예시 5

--- src/prac18/_02_Catch.java
+++ src/prac18/_02_Catch.java
@@ -7,9 +7,8 @@ public class _02_Catch {
       // System.out.println(3/0);
       // int[] arr = new int[3];
       // arr[5] = 100;
-      Object obj = "test";
-      System.out.println((int)obj);
-
+      String s = null;
+      s.toLowerCase();
     } catch (ArithmeticException e) {
       System.out.println("뭔가 잘못 계산을 하셨네요.");
 
@@ -21,8 +20,8 @@ public class _02_Catch {
     }
 
     catch (Exception e) { // Exception 은 모든 클래스의 조상 클래스이다.
-      System.out.println("이런 문제가 발생했어요 => " + e.getMessage());
-      System.out.println();
+      System.out.println("그 외의 모든 에러는 여기서 처리됩니다.");
+      e.printStackTrace();
     }
     System.out.println("프로그램 정상 종료");
   }
