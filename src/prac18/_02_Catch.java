15be693da3deba8a3554ac6a4ba9263d043fdf74
EUIJOON <euijoonism@gmail.com>
2023-03-15 01:08:42 +0900

    [Day18] Catch 예시 3 조금 변형된 내용(두개를 한꺼번에 처리하기) 다시 설명

--- src/prac18/_02_Catch.java
+++ src/prac18/_02_Catch.java
@@ -64,7 +64,7 @@ public class _02_Catch {
     }
     System.out.println("프로그램 정상 종료");
      */
-
+/*
     // 예시 3 수정
     try {
       String s = null;
@@ -84,7 +84,28 @@ public class _02_Catch {
       e.printStackTrace();
     }
     System.out.println("프로그램 정상 종료");
+ */
 
+    // 두 에러를 한꺼번에 처리도 가능
+    try {
+    String s = null;
+    s.toLowerCase();
+
+  } catch (ArithmeticException  | ArrayIndexOutOfBoundsException e) {
+    System.out.println("뭔가 실수하셨네요.");
+
+    // } catch (ArrayIndexOutOfBoundsException e) {
+    //  System.out.println("뭔가 실수하셨네요.");
+
+  } catch (ClassCastException e) {
+    System.out.println("잘못된 형변환입니다.");
+  }
+
+    catch (Exception e) { // Exception 은 모든 클래스의 조상 클래스이다.
+    System.out.println("그 외의 모든 에러는 여기서 처리됩니다.");
+    e.printStackTrace();
+  }
+    System.out.println("프로그램 정상 종료");
   }
 
 }
