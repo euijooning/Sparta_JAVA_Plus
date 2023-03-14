c505524fbbe9664d3b056cd320d91a05c4d79108
EUIJOON <euijoonism@gmail.com>
2023-03-15 00:28:13 +0900

    [Day18] try-catch 구조 설명 부연 및 예시 3개 제대로 작성 보충

--- src/prac18/_01_TryCatch.java
+++ src/prac18/_01_TryCatch.java
@@ -1,3 +1,4 @@
+
 package prac18;
 
 public class _01_TryCatch {
@@ -11,25 +12,62 @@ public class _01_TryCatch {
     // 런타임 오류 예시) int[] arr = new int[3];
     //                 arr[5] = 100; (공간이 벗어남) 실행 시에 오류 발생
 
-    /*
+
+/*
     S18 s = new S18();
       s.methodA();      // 스택오버플로우
-     */
+     *//*
 
-    /*
+
+ */
+/*
     S18 s = null;
     s.methodA(); //NullPointException
-     */
+     *//*
 
-    // 예외처리 문법
+
+ */
+/*
+
+// 예시 1)
     try {
-      Object obj = "test";
-      System.out.println((int)obj);
+      System.out.println(3/0);
     } catch (Exception e) {
       System.out.println("이런 문제가 발생했어요 => " + e.getMessage());
-      System.out.println();
+      e.printStackTrace();
+    }
+    System.out.println("프로그램 정상 종료");
+  }
+// ArithmeticException : / by zero 라는 오류 메시지 출력/
+// 하지만 예외처리 해줬으므로, 프로그램 정상 종료 메시지는 나옴.
+
+// 예시 2)
+try {
+    int[] arr = new int[3]
+    arr[5] = 100;
+  } catch (Exception e) {
+    System.out.println("이런 문제가 발생했어요 => " + e.getMessage());
+    e.printStackTrace();
+  }
+    System.out.println("프로그램 정상 종료");
+}
+// ArrayIndexOutOfBoundsException : Index 5 가 3의 길이를 초과함. 요런 에러 뜬다.
+// 하지만 예외처리 해줬으므로, 프로그램 정상 종료 메시지는 나옴.
+
+
+     */
+
+//예시 3)
+try {
+    Object obj = "test";
+    System.out.println((int)obj);
+    } catch (Exception e) {
+    System.out.println("이런 문제가 발생했어요 => " + e.getMessage());
+    System.out.println();
     }
     System.out.println("프로그램 정상 종료"); // 출력되지 않는다.
-  }
+    }
+
+// ClassCastException : ~ String cannot be cast to Integer 뭐 요런 메시지 나옴.
 }
 
