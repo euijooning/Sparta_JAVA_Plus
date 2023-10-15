6e2ff67bab2f52de1611cbc1322a2689a8053cb8
EUIJOON <euijoonism@gmail.com>
2023-10-15 17:45:24 +0900

    [Day22] thread Quiz

---
+++ src/Prac20/Prac20.java
@@ -0,0 +1,59 @@
+package Prac20;
+
+// A 상품 준비 1/5
+// B 상품 준비 2/5
+//      ...
+// -- A 상품 준비 완료 --
+// -- B 상품 준비 완료 --
+// == 세트 상품 포장 시작 ==
+// 세트 상품 포장 1/5
+//      ...
+// == 세트 상품 포장 완료 ==
+
+public class Prac20 {
+    public static void main(String[] args) {
+        Runnable runnableA = () -> {
+            for (int i = 1; i <=5 ; i++) {
+                System.out.println("A 상품 준비 " + i + "/5");
+            };
+            System.out.println(" -- A 상품 준비 완료 -- ");
+        };
+
+        Runnable runnableB = () -> {
+            for (int i = 1; i <=5 ; i++) {
+                System.out.println("B 상품 준비 " + i + "/5");
+            };
+            System.out.println(" -- B 상품 준비 완료 -- ");
+        };
+
+        Thread threadA = new Thread(runnableA);
+        Thread threadB = new Thread(runnableB);
+
+        threadA.start();
+        threadB.start();
+
+        while (threadA.isAlive() || threadB.isAlive()) {
+        }
+
+//        try {
+//            threadA.join(); 기다리기
+//            threadB.join();
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
+
+
+        Runnable runnableSet = () -> {
+            System.out.println("== 세트 상품 포장 시작 ==");
+            for (int i = 1; i <=5 ; i++) {
+                System.out.println("세트 상품 포장 " + i + "/5");
+            };
+            System.out.println("== 세트 상품 포장 완료 == ");
+        };
+        Thread threadSet = new Thread(runnableSet);
+        threadSet.start();
+    }
+}
+/**
+ * Runnable 객체 3개 만들었다.
+ */
